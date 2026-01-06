44427e46e1a3e6406d665cb741d6fb15ef42c28d
Brady Price <bradyprice1@gmail.com>
2026-01-06 20:28:03 +0000

    does the .env look right and work

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "your_api_key_here",
+  authDomain: "your_auth_domain_here",
+  projectId: "your_project_id_here",
+  storageBucket: "your_storage_bucket_here",
+  messagingSenderId: "your_messaging_sender_id_here",
+  appId: "your_app_id_here",
 };
