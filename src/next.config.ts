d433fe05c84413bbd0556ed58497325fc428c70b
Brady Price <bradyprice1@gmail.com>
2026-01-09 03:19:01 +0000

    can you turn this back to original before 09b0fd31

--- src/next.config.ts
+++ src/next.config.ts
@@ -24,14 +24,8 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
       },
