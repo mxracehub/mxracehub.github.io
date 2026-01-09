566fa408bde87af3809db8113c92218a1b3f71fb
Brady Price <bradyprice1@gmail.com>
2026-01-09 03:21:20 +0000

    can you fix the Footer src/components/layout/footer.tsx  and RootLayout

--- src/next.config.ts
+++ src/next.config.ts
@@ -24,11 +24,17 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
