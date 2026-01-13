e3fa6794f7e1dbafb9edafd20caef71de6bc6d5e
Brady Price <bradyprice1@gmail.com>
2026-01-13 00:39:32 +0000

    can you add the sound button to the youtube

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -133,7 +133,7 @@ export default function DashboardPage() {
       <div className="relative mb-8 overflow-hidden rounded-lg aspect-video">
         <iframe
             className="absolute top-0 left-0 w-full h-full"
-            src="https://www.youtube.com/embed/6RDZJ57BoUU?autoplay=1&mute=1&loop=1&playlist=6RDZJ57BoUU&controls=0&showinfo=0&autohide=1&modestbranding=1"
+            src="https://www.youtube.com/embed/6RDZJ57BoUU?autoplay=1&mute=0&loop=1&playlist=6RDZJ57BoUU&controls=1&showinfo=0&autohide=1&modestbranding=1"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
