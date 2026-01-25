59aae7866d346d4ee9e3624c5d790f1a6627dfd5
Brady Price <bradyprice1@gmail.com>
2026-01-25 17:00:28 +0000

    on page /standings/manufacturers can you update new results from picture

--- src/lib/manufacturers-points-data.ts
+++ src/lib/manufacturers-points-data.ts
@@ -1,14 +1,15 @@
 
 export const manufacturersPoints = [
-    { pos: 1, manufacturer: 'Yamaha', points: 82 },
-    { pos: 2, manufacturer: 'Honda', points: 78 },
-    { pos: 3, manufacturer: 'KTM', points: 73 },
-    { pos: 4, manufacturer: 'Kawasaki', points: 70 },
-    { pos: 5, manufacturer: 'Husqvarna', points: 51 },
-    { pos: 6, manufacturer: 'Suzuki', points: 47 },
-    { pos: 7, manufacturer: 'Ducati', points: 26 },
-    { pos: 8, manufacturer: 'Triumph', points: 9 },
+    { pos: 1, manufacturer: 'Yamaha', points: 124 },
+    { pos: 2, manufacturer: 'Kawasaki', points: 113 },
+    { pos: 3, manufacturer: 'Honda', points: 110 },
+    { pos: 4, manufacturer: 'KTM', points: 107 },
+    { pos: 5, manufacturer: 'Husqvarna', points: 84 },
+    { pos: 6, manufacturer: 'Suzuki', points: 74 },
+    { pos: 7, manufacturer: 'Ducati', points: 37 },
+    { pos: 8, manufacturer: 'Triumph', points: 14 },
     { pos: 9, manufacturer: 'Beta', points: 6 },
     { pos: 10, manufacturer: 'GasGas', points: 4 },
 ];
+
 
